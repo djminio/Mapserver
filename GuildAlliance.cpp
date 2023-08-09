b294f1e0115fe6a37b0441cbd06c5e34d7c70ca5
reece forbes <Mr.Forbes1@hotmail.co.uk>
2023-08-10 00:16:39 +0100

    update for compiling it with the mapserver

--- GuildAlliance.cpp
+++ GuildAlliance.cpp
@@ -1,12 +1,14 @@
 #include"StdAfx.h"
 #include "GuildAlliance.h"
-
+GuildAlliance gc_alliance;
 GANPC::GANPC()
 {
-    g_npcNo = -1;
+    ga_npcNo = -1;
     ga_npcx = 0;
     ga_npcy = 0;
-
+    ga_MapName[29]=0;
+    price=0;
+    nation[2]=0;
 }
 
 GANPC::~GANPC()
@@ -15,12 +17,15 @@ GANPC::~GANPC()
 
 GuildAlliance::GuildAlliance()
 {
-     this->AllianceLevel=0;
-     this->ga_guildcode=0;
-     this->ga_alliancecode=0;
-     this->AllianceLevelup=false;
-     this->AllianceExp=0;
+    this->AllianceLevel=0;
+    this->ga_guildcode=0;
+    this->ga_alliancecode=0;
+    this->ga_AllianceRank=0;
+    this->ga_AllianceLevelup=0;
+    this->ga_AllianceExp=0;
 }
+
+
 
 
 AllianceMGR::AllianceMGR()
@@ -29,4 +34,34 @@ AllianceMGR::AllianceMGR()
 
 AllianceMGR::~AllianceMGR()
 {
+}
+
+
+void AllianceMGR::CheckLevelUp(int cn)
+{
+    if (cn == NULL) return;
+    
+    int currentexp = gc_alliance.Alliancexp();
+    int NextExp = gc_alliance.GetMaxNextAllianceExp();
+    //int expbonus= GetExpBonusRate();
+    /*int exp= GuildAlliance::Alliancexp();*/
+
+    /*int bonus= currentexp* expbonus;*/
+    if (currentexp<NextExp)return;
+
+    //todo need to check Alliance if statement charlist for player
+    if (currentexp >= NextExp)
+    {
+     
+        gc_alliance.AllianceLevel++;
+       gc_alliance.ga_AllianceLevelup = true;
+
+        //to do updatetodbeamon
+        // 
+        // //todo send bbs to all members in the alliance when alliance has leveled up
+        //add log
+        
+    }
+   gc_alliance.ga_AllianceLevelup = false;
+    return;
 }
--- GuildAlliance.h
+++ GuildAlliance.h
@@ -1,8 +1,42 @@
 #pragma once
 
-int MAX_SUB_MASTERS = 4;
-int MAX_GUILD_ALLIANCE = 2;
-class GuildAlliance 
+const int MAX_SUB_MASTERS = 4;
+const int MAX_GUILD_ALLIANCE = 2;
+const int ALLIANCE_COST = 1000000;
+const int BUILD_ALLIANCE_LEVEL = 40; // CSD-030806
+class AllianceMGR
+{
+public:
+    AllianceMGR();
+    virtual ~AllianceMGR();
+    bool LoadGuildMGRTable();
+    static void CheckLevelUp(int cn);
+
+
+    int GetMaxAllianceExp() { ga_max_exp; };
+    static void SetAllianceExp(int exp) { ga_max_exp = exp; };
+
+
+    int GetMaxNextAllianceExp() { return ga_next_exp; };
+    void SetNextAllianceExp(int Nextexp) { ga_next_exp = Nextexp; };
+
+    static int GetExpRate() { ga_exp_rate; };
+    static void SetExpRate(int SetRate) { ga_exp_rate = SetRate; };
+
+    static int GetExpBonusRate() { ga_exp_bonus_rate; };
+    static void SetExpBonusRate(int bonus) { ga_exp_bonus_rate = bonus; };
+
+protected:
+    int Alliancelv;//max 10
+    static int ga_max_exp;//if exp for alliance is 1000 to next level
+    int ga_next_exp;//if exp_next reached 1001
+    static int ga_exp_rate;//how fast you gain exp for alliance
+    static int ga_exp_bonus_rate;//how fast you gain exp for alliance
+
+
+
+};
+class GuildAlliance: public  AllianceMGR
 {
 public:
     enum AllianceRank
@@ -27,73 +61,79 @@ public:
     typedef struct DelAlliance{
         int AllianceCode;
         int AlliancerRank;
+        char AllianceName[100];
     }t_del_Alliance;
 
     typedef struct CreateAlliance {
         
         int guildcode;
+        char AllianceName[100];
 
     }t_Create_Alliance;
 
     typedef struct AllianceChat {
         char AllianceChat[100];
         char guildname[40];
+        int type;
 
     }t_Alliance_chat;
 
+    typedef struct OfficerChat {
+        char AllianceChat[100];
+        char guildname[40];
+        int type;
+
+    }t_Alliance_chat;
     void MakeOfficers(CHARLIST* Player);
 
     GuildAlliance();
-    virtual ~GuildAlliance();
+   // virtual ~GuildAlliance();
     //maybe todo sepecial shop for alliance and maybe stalls for differnt alliance controlled by them??
     static bool LoadAllianceTable();
 
-    bool AllianceLevelUp() { return AllianceLevelup; };//check if alliance leveled up yet
+    bool AllianceLevelUp() { return ga_AllianceLevelup; };//check if alliance leveled up yet
+   int  Alliancexp() { return ga_AllianceExp; };//curr exp
+    void setAllianceExp(int exp) { ga_AllianceExp = exp; };// we set what exp the alliance gets reece
+    void CheckLevelUp(int cn);
 
-   static int  Alliancexp() { return AllianceExp; };//curr exp
-   static  int setAllianceExp(int exp) { AllianceExp = exp; };// we set what exp the alliance gets reece
+   void SetAllianceCode(int code) { ga_alliancecode = code; };
+   int GetAllianceCode() { return ga_alliancecode; };
 
-    int SetAllianceCode();
-  
-
-    int setAllianceRank(int rank) { AllianceRank = rank; };
-    int GetAllianceRank() { return AllianceRank; };
+    void setAllianceRank(int rank) { ga_AllianceRank = rank; };
+    int GetAllianceRank() { return ga_AllianceRank; };
 
     void checkforDuplication();
     int getAllianceCode();
     int CreateAllianceCode();
 
     int getAllianceLevel() { return AllianceLevel;};
-    int setAllianceLevel(int level) { AllianceLevel = level; };
+    void setAllianceLevel(int level) { AllianceLevel = level; };
 
    
     
     string AllianceLevelBBS(int cn,string& s);
     void MakeSubMasters(CHARLIST* Player);
     //functions for the packets mapserver to client and viceversa
-    void AllianceChat();
-    void AllianceOfficerChat();
-protected:
+    void AllianceChat(packet* p);
+    void AllianceOfficerChat(packet* p);
+    void SendAllianceChat(t_Alliance_chat*p);
+    void SendOfficerChat(t_Alliance_chat* p);
+
+public:
     int AllianceLevel;
+    bool ga_AllianceLevelup;
+protected:
+    int ga_AllianceExp;
+    
+
     int ga_guildcode;
     int ga_alliancecode;
-    int AllianceRank;
-    bool AllianceLevelup;
-   static  int AllianceExp;
-};
-class AllianceMGR
-{
-public:
-    AllianceMGR();
-   virtual ~AllianceMGR();
-   bool LoadGuildMGRTable();
-protected:
-    int Alliancelv;//max 10
-    int max_exp;//if exp for alliance is 1000 to next level
-    int next_exp;//if exp_next reached 1001
-    int exp_rate;//how fast you gain exp for alliance
+    int ga_AllianceRank;
+   
+  
 };
 
+//extern int GetMaxNextAllianceExp();
 
 class GANPC
 {
@@ -109,15 +149,17 @@ public:
     //receive from client reece
     void RecvCreateAlliance(packet* p);
     void RecvDeleteAlliance(packet* p);
-
+    //todo alliance shop maybe?
     //send back to the client when all is done reece
     void SendDeleteAlliance(packet* p);
     void SendCreateAlliance(packet* p);
 protected:
-    int g_npcNo;
+    int ga_npcNo;
     int ga_npcx;
     int ga_npcy;
     char ga_MapName[30];
+    int price;
+    int nation[3];
 };
 
 
